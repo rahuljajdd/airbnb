45c72ea867332ce6f45521c7e6d9cd59e9a0ee36
rahuljajdd <rs3296471t@gmail.com>
2024-09-20 13:12:21 +0530

    ddfd

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { Prisma ,PrismaClient} from "@prisma/client";
 
 
-import { MongoClient } from "mongodb"
+// import { MongoClient } from "mongodb"
 
 
 
